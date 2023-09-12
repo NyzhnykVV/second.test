553a2ef45569b65dd12f7d5a8aa8558a011b8340
NyzhnykVV <144223356+NyzhnykVV@users.noreply.github.com>
2023-09-12 19:51:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = 17
+let a = NaN
+let Text = "Sup!"
+let TrueFalse = true
+let b = undefined
+
+let Me = {
+    Name: "Volodia",
+    Surname: "Nuzhnyk",
+    Age: "17",
+    City: "Tlumach"
+}
+
+console.log(number, a, Text, TrueFalse, b)
+console.table(Me)
